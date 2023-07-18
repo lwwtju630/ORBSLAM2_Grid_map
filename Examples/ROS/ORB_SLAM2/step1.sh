f147a5d3ae3210cb96bcf3638c95ac03598fe8f7
lwwtju630 <lwwtju630@gmail.com>
2023-07-18 13:53:00 +0800

    orbslam2 output Occupancy grid map

---
+++ Examples/ROS/ORB_SLAM2/step1.sh
@@ -0,0 +1,3 @@
+#https://blog.csdn.net/mmeeed/article/details/124283775
+
+rosrun ORB_SLAM2 Monopub ../../../Vocabulary/ORBvoc.txt ../../../Examples/Monocular/demo_cam.yaml -1 /camera/rgb/image_color
---
+++ Examples/ROS/ORB_SLAM2/step2.sh
@@ -0,0 +1,1 @@
+rosrun ORB_SLAM2 Monosub 30 2 6 -6 6 -6 0.55 0.50 1 5
